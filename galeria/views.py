5d7ed1387f23afa155cae800ccf844e954ad511d
Cayke Bispo <Cayke.teixeira@bcrcx.com>
2026-05-29 17:29:22 -0300

    Add galeria app with initial structure and settings updates

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {"nome": "nebulosa de carina",
+            "legenda": "webtelecope.org/ NASA / James Webb"},
+        2: { "nome": "Galáxia NGC 1079",
+            "legenda": "webtelecope.org/ NASA / James Webb"},
+    }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
